babd200fdc331ebbef8e5c9607b22bd0c31bb3d9
trig0n <pascal@eberlein.io>
2019-06-10 17:13:20 +0200

    update

--- updater.py
+++ updater.py
@@ -90,8 +90,8 @@ class Updater(Thread):
     @staticmethod
     def get_ip():
         d = get("http://checkip.dyndns.com/")
-        d = d.content.split(u":")[1]
-        d = d.split(u"<")[0]
+        d = d.content.split(b":")[1]
+        d = d.split(b"<")[0]
         d.replace(' ', '')
         return d
 
