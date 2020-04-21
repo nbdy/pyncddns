f17f8d6e96886d31d293a71d76a48c8d94096371
smthnspcl <pascal@eberlein.io>
2020-04-21 22:00:31 +0200

    update

--- updater.py
+++ updater.py
@@ -1,64 +1,9 @@
 #!/usr/bin/python3
 
 from requests import get
-from sys import argv
-from time import sleep
+from time import sleep, time
 from threading import Thread  # could use Timer but imagine how much less code that would be
-
-
-class Configuration(object):
-    ip = None
-    hosts = []
-    domain = None
-    password = None
-    sleep_time = 60*60
-    verbose = False
-
-    @staticmethod
-    def help():
-        print("usage: updater.py {arguments}")
-        print("[arguments]")
-        print("\t-i\t--ip")
-        print("\t-h\t--host")
-        print("\t-d\t--domain")
-        print("\t-p\t--password")
-        print("\t-s\t--sleep-time\tdefault: 3600")
-        print("\t-v\t--verbose")
-        print("\t--help")
-        exit()
-
-    @staticmethod
-    def parse():
-        cfg = Configuration()
-        i = 0
-        while i < len(argv):
-            if argv[i] in ["-i", "--ip"]:
-                cfg.ip = argv[i + 1]
-            elif argv[i] in ["-h", "--host"]:
-                cfg.hosts.append(argv[i + 1])
-            elif argv[i] in ["-d", "--domain"]:
-                cfg.domain = argv[i + 1]
-            elif argv[i] in ["-p", "--password"]:
-                cfg.password = argv[i + 1]
-            elif argv[i] in ["-s", "--sleep-time"]:
-                cfg.sleep_time = int(argv[i + 1])
-            elif argv[i] in ["-v", "--verbose"]:
-                cfg.verbose = True
-            elif argv[i] in ["--help"]:
-                Configuration.help()
-            i += 1
-
-        if len(cfg.hosts) == 0:
-            print("host list is empty")
-            exit()
-        if cfg.domain is None:
-            print("what domain should i use?")
-            exit()
-        if cfg.password is None:
-            print("not sure if you can log in without a password")
-            exit()
-
-        return cfg
+from argparse import ArgumentParser
 
 
 class Updater(Thread):
@@ -84,18 +29,20 @@ class Updater(Thread):
                     self.update(self.ip, host, self.config.domain, self.config.password)
             if self.config.verbose:
                 print("sleeping for", self.config.sleep_time, "seconds")
-            sleep(self.config.sleep_time)
+            self.sleep_cycle()
+
+    def sleep_cycle(self):
+        ss = time()
+        while (time() - ss) < self.config.sleep_time:
+            if not self.do_run:
+                break
+            sleep(1)
 
     def stop(self):
         self.do_run = False
 
-    @staticmethod
-    def get_ip():
-        d = get("http://checkip.dyndns.com/")
-        d = d.content.split(b':')[1]
-        d = d.split(b'<')[0]
-        d.replace(b' ', b'')
-        return d
+    def get_ip(self):
+        return get(self.config.ip_site).content
 
     @staticmethod
     def update(ip, host, domain, password):
@@ -111,8 +58,26 @@ class Updater(Thread):
 
 
 if __name__ == '__main__':
-    u = Updater(Configuration.parse())
+    ap = ArgumentParser()
+    ap.add_argument("-i", "--ip", help="does not need to be set, will be fetched from the specified site")
+    ap.add_argument("--host", action="append", nargs="+", help="subdomains to update")
+    ap.add_argument("-d", "--domain", type=str, required=True, help="tld under which the subdomains run")
+    ap.add_argument("-p", "--password", type=str, required=True, help="the dyndns password")
+    ap.add_argument("-s", "--sleep-time", default=3600, type=int,
+                    help="time between checking ip and updating if ip has changed")
+    ap.add_argument("-v", "--verbose", action="store_true", help="be more verbose")
+    ap.add_argument("--ip-site", type=str, default="https://api.ipify.org",
+                    help="site on which to fetch the current ip; needs to be plain text")
+    a = ap.parse_args()
+
+    a.hosts = []
+    for h in a.host:
+        a.hosts.append(h[0])
+
+    u = Updater(a)
     try:
         u.start()
+        u.join()
     except KeyboardInterrupt:
+        print("\ncaught ctrl+c; stopping")
         u.stop()
